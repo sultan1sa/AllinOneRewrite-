71ea8b8144c640ac346b08f485cd6b847ee889bb
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-09-05 11:33:24 +0300

    Update ChatUncensored.js

--- ChatUncensored.js
+++ ChatUncensored.js
@@ -1,71 +1,65 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-var header = $request.headers;
-
-obj = {
-"data": {
-"type": "adapty_analytics_profile",
-"id": $request.headers["adapty-sdk-profile-id"],
-"attributes": {
-    "app_id": "20ed8826-ed31-44ad-a0b3-43c8d360d393",
-    "profile_id" : $request.headers["adapty-sdk-profile-id"],
-    "customer_user_id": "6FCDBCBC-E369-4A1F-AE94-C898475A813C",
-    "total_revenue_usd": 0.0,
-    "segment_hash": "f29021fa71267408",
-    "paid_access_levels": {
+{
+  "data": {
+    "type": "adapty_analytics_profile",
+    "id": "d75ea8a3-1baf-42e3-94ab-204c9d4cba13",
+    "attributes": {
+      "app_id": "20ed8826-ed31-44ad-a0b3-43c8d360d393",
+      "profile_id": "d75ea8a3-1baf-42e3-94ab-204c9d4cba13",
+      "customer_user_id": "6FCDBCBC-E369-4A1F-AE94-C898475A813C",
+      "total_revenue_usd": 0,
+      "segment_hash": "f29021fa71267408",
+      "paid_access_levels": {
         "premium": {
-            "id": "premium",
-            "is_active": true,
-            "is_lifetime": true,
-            "expires_at": "9692-12-26T16:30:25.000000+0000",
-            "starts_at": null,
-            "will_renew": true,
-            "vendor_product_id": "cgptu_799_1w",
-            "store": "app_store",
-            "activated_at": "2023-12-23T16:30:26.000000+0000",
-            "renewed_at": "2023-12-23T16:30:25.000000+0000",
-            "unsubscribed_at": null,
-            "billing_issue_detected_at": null,
-            "is_in_grace_period": false,
-            "active_introductory_offer_type": "free_trial",
-            "offer_id": null,
-            "active_promotional_offer_type": null,
-            "active_promotional_offer_id": null,
-            "cancellation_reason": null,
-            "is_refund": false
+          "id": "premium",
+          "is_active": true,
+          "is_lifetime": false,
+          "expires_at": "9692-12-26T16:30:25.000000+0000",
+          "starts_at": null,
+          "will_renew": true,
+          "vendor_product_id": "cgptu_1499_1mo",
+          "store": "app_store",
+          "activated_at": "2023-12-23T16:30:26.000000+0000",
+          "renewed_at": "2023-12-23T16:30:25.000000+0000",
+          "unsubscribed_at": null,
+          "billing_issue_detected_at": null,
+          "is_in_grace_period": false,
+          "active_introductory_offer_type": "free_trial",
+          "offer_id": null,
+          "active_promotional_offer_type": null,
+          "active_promotional_offer_id": null,
+          "cancellation_reason": null,
+          "is_refund": false
         }
-    },
-    "subscriptions": {
-        "cgptu_799_1w": {
-            "is_active": true,
-            "is_lifetime": true,
-            "expires_at": "9692-12-26T16:30:25.000000+0000",
-            "starts_at": null,
-            "will_renew": true,
-            "vendor_product_id": "cgptu_799_1w",
-            "vendor_transaction_id": "420001719421278",
-            "vendor_original_transaction_id": "420001719421278",
-            "store": "app_store",
-            "activated_at": "2023-12-23T16:30:26.000000+0000",
-            "renewed_at": "2023-12-23T16:30:25.000000+0000",
-            "unsubscribed_at": null,
-            "billing_issue_detected_at": null,
-            "is_in_grace_period": false,
-            "active_introductory_offer_type": "free_trial",
-            "offer_id": null,
-            "active_promotional_offer_type": null,
-            "active_promotional_offer_id": null,
-            "cancellation_reason": null,
-            "is_sandbox": false,
-            "is_refund": false
+      },
+      "subscriptions": {
+        "com.Chat - GPT - Uncensored": {
+          "is_active": true,
+          "is_lifetime": false,
+          "expires_at": "9692-12-26T16:30:25.000000+0000",
+          "starts_at": null,
+          "will_renew": true,
+          "vendor_product_id": "xremover.instants.oneYear",
+          "vendor_transaction_id": "420001719421278",
+          "vendor_original_transaction_id": "420001719421278",
+          "store": "app_store",
+          "activated_at": "2023-12-23T16:30:26.000000+0000",
+          "renewed_at": "2023-12-23T16:30:25.000000+0000",
+          "unsubscribed_at": null,
+          "billing_issue_detected_at": null,
+          "is_in_grace_period": false,
+          "active_introductory_offer_type": "free_trial",
+          "offer_id": null,
+          "active_promotional_offer_type": null,
+          "active_promotional_offer_id": null,
+          "cancellation_reason": null,
+          "is_sandbox": false,
+          "is_refund": false
         }
-    },
-    "non_subscriptions": null,
-    "custom_attributes": {},
-    "promotional_offer_eligibility": false,
-    "introductory_offer_eligibility": false
+      },
+      "non_subscriptions": null,
+      "custom_attributes": {},
+      "promotional_offer_eligibility": false,
+      "introductory_offer_eligibility": false
+    }
+  }
 }
-}
-};
-body = JSON.stringify(obj);
-$done({body:body});
